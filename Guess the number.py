ea23b3566537a2b8cfb692ea35cf7043a5bea679
Patrick Tran <tranpatrick01@gmail.com>
2017-08-04 12:02:09 -0500

    Add guess counter for Player

--- Guess the number.py
+++ Guess the number.py
@@ -2,22 +2,25 @@ import random
 a = random.randint(0,200)
 print(a)
 guess = int(input('Guess a number from 1-200: '))
+counterPlayer = 0
 while True:
     if guess == a:
         print('You guessed the number')
         break
     elif guess > a:
         print('You guessed too high. Try again.')
-        guess = int(input('Guess again'))
+        guess = int(input('Guess again: '))
+        counterPlayer += 1
     elif guess < a:
         print('You guess too low. Try again.')
-        guess = int(input('Guess again'))
-    
+        guess = int(input('Guess again: '))
+        counterPlayer += 1
+print('It took you: ' + str(counterPlayer) + ' guessed the number')
             
 
 
 #*****TO DO****#
 #add computer guess AI#
-#add guess counter for user and AI#
+#add guess  AI#
 #compare guess counter to see who won#
 
